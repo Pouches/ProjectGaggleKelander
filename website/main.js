80dfff2ab1865311557c7bc02fa51083c0db1f4f
Cade Brown <cadebrown1776@gmail.com>
2021-03-10 15:21:51 -0700

    modified:   website/main.js

--- website/main.js
+++ website/main.js
@@ -1,3 +1,5 @@
+//Created By Cade Brown, on 03/07/2021 at 1:04pm    
+//Project: Create a website and javascript(in nodeJS) to connect to google calendar for display, creation, and deletion of events
 const {google, GoogleApis} = require('googleapis');
 const express = require('express');
 const SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -10,18 +12,18 @@ const REDIRECT = keys.REDIRECT_URI;//stored in json
 const KEY = keys.API_KEY;//stored in json
 const oAuth2Client = new google.auth.OAuth2(ID,SECRET,REDIRECT);
 oAuth2Client.setCredentials({refresh_token:REFRESH});
+//------------------------------------------------------------------------------------------------------------------------------------------
+//Lists next 10 upcoming events
 async function EventList(auth){
     try{const accessToken = await oAuth2Client.getAccessToken();
-    //console.log(accessToken);
-    const Calendar = google.calendar({version:'v3', auth});
-    //console.log(Calendar.calendarList.list());
-    Calendar.events.list({
-        calendarId:'cbrown199@west-mec.org',
-        timeMin:(new Date()).toISOString(),
-        maxResults:10,
-        singleEvents:true,
-        orderBy:'startTime',
-        key:KEY,
+        const Calendar = google.calendar({version:'v3', auth});
+        Calendar.events.list({
+          calendarId:'cbrown199@west-mec.org',
+          timeMin:(new Date()).toISOString(),
+          maxResults:10,
+          singleEvents:true,
+          orderBy:'startTime',
+          key:KEY,
         },(err,res)=>{
                 if(err)return console.log('The API returned an error:'+ err);
                 const events = res.data.items;
@@ -39,4 +41,99 @@ async function EventList(auth){
                 }
                 else{console.log('No upcoming events found')}});}
             catch(error){return error;}}
-EventList(oAuth2Client);
+//------------------------------------------------------------------------------------------------------------------------------------------
+//Creates a new event            
+async function CreateEvent(auth){
+    try{const accessToken = await oAuth2Client.getAccessToken();
+        const Calendar = google.calendar({version:'v3', auth});
+        Calendar.events.insert({
+            calendarId:'cbrown199@west-mec.org',
+            key:KEY,
+            resource: Event
+        },function(err, event){
+            if(err){console.log(err)};
+            console.log(`Event created: ${event}`)
+        })
+    
+    }
+    catch(error){return error;}}
+//------------------------------------------------------------------------------------------------------------------------------------------
+//Deletes and event
+async function DeleteEvent(auth){
+  try{const accessToken = await oAuth2Client.getAccessToken
+      const Calendar = google.calendar({version:'v3', auth});
+      Calendar.events.delete({
+          calendarId:'cbrown199@west-mec.org',
+          key:KEY,
+      })
+  }
+  catch(error){return error;}}
+//------------------------------------------------------------------------------------------------------------------------------------------
+CreateEvent(oAuth2Client)
+EventList(oAuth2Client)
+//------------------------------------------------------------------------------------------------------------------------------------------
+/*Note book
+function skeleton:
+
+async function Name(auth){
+    try{const accessToken = await oAuth2Client.getAccessToken
+        const Calendar = google.calendar({version:'v3', auth});
+        Calendar.something.somethingElse({
+            calendarId:'cbrown199@west-mec.org',
+            key:KEY,
+        })
+    }
+    catch(error){return error;}}
+
+
+    calendar event, taken from google's website resource https://developers.google.com/calendar/create-events#node.js :
+    var event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2015-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+};
+
+        var Event = {
+          'summary': 'Test',
+          'location': 'West-MEC NEC',
+          'description': 'I am creating a new event via powershell',
+          'start': {
+            'dateTime':'2021-03-10T09:00:00-16:00:00',
+            'timeZone': 'America/Phoenix'},
+          'end':{
+            'dateTime': '2021-03-10T09:00:00-16:00:00',
+            'timeZone':'America/Phoenix'}
+        }
+        var event = {
+          'summary': 'Google I/O 2015',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'A chance to hear more about Google\'s developer products.',
+          'start': {
+            'dateTime':'2021-03-10T09:00:00-16:00:00',
+            'timeZone': 'America/Phoenix'},
+          'end':{
+            'dateTime': '2021-03-10T09:00:00-16:00:00',
+            'timeZone':'America/Phoenix'}};
+*/
