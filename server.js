c91f10fa489f73981b3bc773303278b6ebbb4d67
Cade Brown <cadebrown1776@gmail.com>
2021-03-12 14:46:41 -0700

    modified:   server.js; 	new file:   website/CSS/fonts/borda_medium.ttf; 	modified:   website/CSS/good_style.css; 	new file:   website/images/TCTD2_Icon_Phoenix_Secondary_Orange_208x208.png; 	modified:   website/index.html;

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const nodemon = require('nodemon');
 const {JSDOM} = require('jsdom');
+const bodyParser = require('body-parser');
 const CalendarUsage = require('./website/main')//contains all the Google apis and function to access them
 ////----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Creates a HTTP server
@@ -10,10 +11,12 @@ const app = express();
 app.use(express.static(`${__dirname}/website`));
 app.get('/', function(req,res){
   res.sendFile(`${__dirname}/index.html`);
-  
   });
-
   app.listen(3000);
+  app.use(bodyParser.urlencoded({
+    extended:false
+  }));
+  app.use(bodyParser.json());
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //10 seconds delay between checks of the Calendar event list 
 setInterval(()=>{
@@ -22,4 +25,16 @@ setInterval(()=>{
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //create an event using the webpage
 //                       (auth,EventSummary,EventLocation,EventDescription,EventDate,StartTime,EndTime)
-CalendarUsage.CreateEvent(CalendarUsage.oAuth2Client)
+//CalendarUsage.CreateEvent(CalendarUsage.oAuth2Client)
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Retrieves the form info
+app.post('/create_event', (req,res)=>{
+  let NewEventSummary = req.body.newEventTitle;
+  let NewEventDate = req.body.newEventDate;
+  let NewEventStart = req.body.newEventStart;
+  let NewEventEnd = req.body.newEventEnd;
+  let NewEventDescription = req.body.newEventDescription;
+  let NewEventLocation = req.body.newEventLocation;
+  CalendarUsage.CreateEvent(CalendarUsage.oAuth2Client, NewEventSummary,NewEventLocation,NewEventDescription,NewEventDate,NewEventStart,NewEventEnd);
+  res.sendfile('website/index.html');
+});
